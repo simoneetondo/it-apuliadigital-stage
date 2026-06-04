9a070a7fef5964fd7d23db6d534c52570baa9e13
Simone Tondo <tr.simonetondo@gmail.com>
2026-06-04 11:52:42 +0200

    Delete esercitazioni/libManag-Quarkus directory

--- esercitazioni/libManag-Quarkus/src/main/java/it/exprivia/exception/BookNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package it.exprivia.exception;
-
-public class BookNotFoundException extends ServiceException {
-    public BookNotFoundException(String isbn) {
-        super("Book with ISBN: " + isbn + " not found.", 404);
-    }
-}
--- esercitazioni/libManag-Quarkus/src/main/java/it/exprivia/exception/DuplicatedBookException.java
+++
@@ -1,8 +0,0 @@
-package it.exprivia.exception;
-
-public class DuplicatedBookException extends ServiceException {
-    public DuplicatedBookException(String isbn) {
-
-        super("Book already exists with ISBN: " + isbn + ".", 409);
-    }
-}
